e8e970d7246d2a903f4c6bf47c3890ba08396759
Michael Dojčár <michaeldojcar@gmail.com>
2020-08-25 02:05:05 +0200

    basic ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="container-fluid">
-    Projekce ProScholy.cz
+  <div class="container-fluid mt-3">
+    <h3>Projekce ProScholy.cz</h3>
+
+    <p>Přihlášený uživatel: Michael Dojčár</p>
+
+    <div class="card">
+      <div class="card-body">
+        <p>Vítejte v systému pro hudební projekci ProScholy.cz</p>
+        <p>Mód: <b class="text-success">{{ state.online_state }}</b> (offline mód je k dispozici)</p>
+        <p>Poslední aktualizace databáze: 25.8. 2020 2:04 - <a>aktualizovat písně nyní</a></p>
+
+        <a class="btn btn-secondary">Vytvořit promítání</a>
+        <a class="btn btn-secondary">Připojit se k existujícímu promítání</a>
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import state from './state'
 
-export default Vue.extend({})
+export default {
+  components: {
+    state
+  },
+
+  data: ()=> {
+    return {
+      state: state
+    }
+  }
+}
 </script>
 
 <style>
---
+++ pages/state.js
@@ -0,0 +1,10 @@
+export default {
+  online_state: 'online',
+
+  songs: [
+    {
+      'name': 'Ať srdce mé Tebe vídá',
+      'lyrics': 'Ať srdce mé Tebe vídá, ať srdce mé tebe zná.'
+    }
+  ]
+}
