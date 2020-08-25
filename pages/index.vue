a26c197da68bdfaf63ce04caedf579c61f86b3dc
Michael Dojčár <michaeldojcar@gmail.com>
2020-08-25 09:09:47 +0200

    index ui

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
       <div class="card-body">
         <p>Vítejte v systému pro hudební projekci ProScholy.cz</p>
         <p>Mód: <b class="text-success">{{ state.online_state }}</b> (offline mód je k dispozici)</p>
-        <p>Poslední aktualizace databáze: 25.8. 2020 2:04 - <a>aktualizovat písně nyní</a></p>
+        <p>Poslední aktualizace databáze: 25.8. 2020 2:04 - <a href="#">aktualizovat písně nyní</a></p>
 
         <a class="btn btn-secondary">Vytvořit promítání</a>
         <a class="btn btn-secondary">Připojit se k existujícímu promítání</a>
@@ -37,3 +37,4 @@ export default {
 <style>
 
 </style>
+
